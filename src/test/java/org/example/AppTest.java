772d05d6da1c6c855a19f456d6a3d2f020ebae6f
Kubatbekov Nurlan <kubatbekovnurlan13@gmail.com>
2022-12-04 02:44:26 +0600

    Third commit: added Test

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,38 +1,43 @@
 package org.example;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class AppTest {
+
+    private final VariableKeeper variableKeeper = new VariableKeeper(78945, 4);
+    private final DividingNumberService dividingNumberService = new DividingNumberService(variableKeeper);
+
+    @Test
+    void firstTest() {
+        ArrayList<String> allResults = dividingNumberService.getResultsOfOperation();
+        ArrayList<String> expectedResults = new ArrayList<>(
+                Arrays.asList("78945", "4", "19736", "7", "4", "38", "36",
+                        " 29", " 28", "  14", "  12", "   25", "   24", "    1")
+        );
+        assertEquals(expectedResults, allResults);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    public void secondTest() {
+        ArrayList<Integer> whereNumberStarts = dividingNumberService.getWhereNumberStarts();
+        ArrayList<Integer> expectedWhereNumberStarts = new ArrayList<>(
+                Arrays.asList(0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 2, 3, 3, 4)
+        );
+        assertEquals(expectedWhereNumberStarts, whereNumberStarts);
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void thirdTest() {
+        ArrayList<Integer> whereNumberEnd = dividingNumberService.getWhereNumberEnd();
+        ArrayList<Integer> expectedWhereNumberEnd = new ArrayList<>(
+                Arrays.asList(4, 0, 4, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 4)
+        );
+        assertEquals(expectedWhereNumberEnd, whereNumberEnd);
     }
+
 }
