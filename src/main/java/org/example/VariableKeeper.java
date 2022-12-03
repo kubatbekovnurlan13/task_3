a8fd4438e97e9ff89c3865f4046f8445fa1cb55e
Kubatbekov Nurlan <kubatbekovnurlan13@gmail.com>
2022-12-04 03:39:48 +0600

    Fourth commit: remove unnecessary methods

--- src/main/java/org/example/VariableKeeper.java
+++ src/main/java/org/example/VariableKeeper.java
@@ -13,15 +13,8 @@ public class VariableKeeper {
         return dividend;
     }
 
-    public void setDividend(int dividend) {
-        this.dividend = dividend;
-    }
-
     public int getDivider() {
         return divider;
     }
 
-    public void setDivider(int divider) {
-        this.divider = divider;
-    }
 }
